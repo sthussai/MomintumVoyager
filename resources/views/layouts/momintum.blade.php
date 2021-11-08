4af67bea872067583de87c1db313ba10a8f14044
Syed Hussain <tekken482@hotmail.com>
2021-11-07 20:06:07 -0700

    laravel 8 upgrade

--- resources/views/layouts/momintum.blade.php
+++ resources/views/layouts/momintum.blade.php
@@ -6,6 +6,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-blue-grey.css">
